5f2c80f00a0c36426f7e1c634dfbc27825af799b
santosh1426 <66237665+santosh1426@users.noreply.github.com>
2020-06-01 11:09:40 +0530

    my capston project one

---
+++ Capston project-1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This  notebook is mainly used for capston python project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
